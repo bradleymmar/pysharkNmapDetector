d43a8b6730331059427deb0b7d4536377ed186e5
bradleymmar <71107579+bradleymmar@users.noreply.github.com>
2024-10-17 16:56:57 -0500

    Update detectory.py
    no more fkn false positives lol

--- detectory.py
+++ detectory.py
@@ -1,45 +1,49 @@
+#!/usr/bin/env python3
 import pyshark
 
 
-fileclear=open('discovered.txt', 'w')
-fileclear.write("")
-fileclear.close()
-file1=open('discovered.txt', 'a')
 
-pcap = input("Please input a full file path (If on windows use double backslashes and no quotations): ")
+file = "/home/bradleymmar/Downloads/SS.pcapng"
+pcap = pyshark.FileCapture(input_file=file)
+detection = 0
 
+scans = {
 
-
-tcp_scan = pyshark.FileCapture(input_file=pcap, display_filter='tcp and tcp.flags == 0x014')
-stealth = pyshark.FileCapture(input_file=pcap, display_filter='tcp and tcp.flags == 0x004')
-fin = pyshark.FileCapture(input_file=pcap, display_filter='tcp and tcp.flags.fin')
-null = pyshark.FileCapture(input_file=pcap, display_filter='tcp and tcp.flags==0x000')
-udp = pyshark.FileCapture(input_file=pcap, display_filter='icmp.code == 3')
+    'tcp_sS':["0x0002", "0x0014"],
 
 
-for pkt in tcp_scan:
-    file1.write("TCP Connect: " )
-    file1.write(pkt.number)
-    file1.write("\n")
+}
 
-for pkt in stealth:
-    file1.write("TCP Stealth: " )
-    file1.write(pkt.number)
-    file1.write("\n")
+pktNum = 0
+pktStreamDict = {}
 
-for pkt in fin:
-    file1.write("TCP Fin: " )
-    file1.write(pkt.number)
-    file1.write("\n")
+for pkt in pcap:
+    pktNum= pktNum+1
 
-for pkt in null:
-    file1.write("TCP Null/XMAS: " )
-    file1.write(pkt.number)
-    file1.write("\n")
 
-for pkt in udp:
-    file1.write("UDP: ")
-    file1.write(pkt.number)
-    file1.write("\n")
+packets=range(0,pktNum,1)
 
-print("All done!")
+for pktNum in packets:
+    pack = pcap[pktNum].tcp.stream
+    pktStreamDict.setdefault(pack, [])
+    pktStreamDict[pack].append(pcap[pktNum].frame_info.number)
+
+
+for pack in pktStreamDict:
+    pkt = pktStreamDict[pack]
+    flagList = []
+    tempStream=pktStreamDict[pack]
+    for i in tempStream:
+        i = int(i)-1
+        pktFlag=pcap[i].tcp.flags
+        flagList.append(pktFlag)
+    for i in scans.keys():
+        if flagList == scans[i]:
+            detection = detection+1
+
+
+
+
+            
+print(detection)
+print(flagList)
